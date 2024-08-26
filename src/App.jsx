3597b8651542392ddd7531585f0757e0deba2f2c
Anna Kotvitska <annakotvitska123@gmail.com>
2024-08-26 22:58:09 +0300

    added  API endpoint (use: mockapi.io)

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,30 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import ProductBlock from './components/ProductBlock';
-import products from './db.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch('https://66cc799fa4dd3c8a71b7bffd.mockapi.io/items')
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
+
   return (
     <div className='wrapper'>
       <Header />
       <div className='content'>
         <div className='container'>
           <div className='content__top'>
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className='content__title'>Вся продукція</h2>
           <div className='content__items'>
-            {products.map((obj) => (
+            {items.map((obj) => (
               <ProductBlock key={obj.id} {...obj} />
             ))}
           </div>
